2f507459f58a30d9e258927363a6b0f3f7b31f98
coderNp101 <67624456+coderNp101@users.noreply.github.com>
2023-08-14 12:52:31 +0545

    Update cash.c

--- cash.c
+++ cash.c
@@ -17,39 +17,63 @@ int main(void)
 	
 	while (cnt != 0)
 	{
-		if (cnt >= 50)
+	    if (cnt >= 1000)
+		{
+			cnt -= 1000;
+			x++;
+			printf("1000, ");
+		}
+	    else if (cnt >= 500)
+		{
+			cnt -= 500;
+			x++;
+			printf("500, ");
+		}
+	    else if (cnt >= 100)
+		{
+			cnt -= 100;
+			x++;
+			printf("100, ");
+		}
+		else if (cnt >= 50)
 		{
 			cnt -= 50;
 			x++;
+			printf("50, ");
 		}
 		else if (cnt >= 20)
 		{
 			cnt -= 20;
 			x++;
+			printf("20, ");
 		}
 		else if (cnt >= 10)
 		{
 			cnt -= 10;
 			x ++;
+			printf("10, ");
 		}
 		else if (cnt >= 5)
 		{
 			cnt -= 5;
 			x ++;
+			printf("5, ");
 		}
 		else if (cnt >= 2)
 		{
 			cnt -= 2;
 			x++;
+			printf("2, ");
 		}
 		else
 		{
 			cnt--;
 			x++;
+			printf("1, ");
 		}
 	}
 	
-	printf("%d\n", x);
+	printf("\nThe total number of individual cash is %d.", x);
 	//getch();
 	
 	return 0;
