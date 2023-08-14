175958b6475bae53188e7081f87189b9cd8fac88
coderNp101 <67624456+coderNp101@users.noreply.github.com>
2023-08-14 13:08:51 +0545

    Add files via upload

---
+++ 3_itym.py
@@ -0,0 +1,27 @@
+
+
+from math import floor
+from math import log2
+
+#I have figured out that as block increases, it depends on the previous block + a predefined pattern
+
+def a(n, k, x): #Here I have defined a function that does the recursion...
+    if k == 2:
+        x.append(floor(n/2))
+        return
+    else:
+        for i in range (floor(n/2), pow(2, k-2) -1, -1):
+            a(i, k-1, x)
+
+
+
+
+for n in range(1, 101):
+    k = floor(log2(2*n))
+    x = []
+
+    for j in range(k, 1, -1):
+        a(n, j, x)
+
+    s = sum(x)+1
+    print("a("+str(n).strip()+") =",s)
